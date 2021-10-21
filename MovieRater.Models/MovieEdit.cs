e1c1cc979d2cd53f1b163e7726808f66aa8e99bd
Jesse Southerland <jessemsoutherland@gmail.com>
2021-10-21 16:59:04 -0400

    movieprops

--- MovieRater.Models/MovieEdit.cs
+++ MovieRater.Models/MovieEdit.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MovieRater.Services
+namespace MovieRater.Models
 {
-    public class Class1
+    class MovieEdit
     {
     }
 }
--- MovieRater.Models/MovieList.cs
+++ MovieRater.Models/MovieList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MovieRater.Models
 {
-    public class Class1
+    class MovieList
     {
     }
 }
