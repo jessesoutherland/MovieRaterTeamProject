571c39bce6e4b6b2cd013d89a6e7b8bac82e6c3e
Ciara Brown <cnbrown2021@gmail.com>
2021-10-21 16:51:09 -0400

    add TvShow data

---
+++ MovieRater.Data/TvShow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Data
+{
+    public class TvShow
+    {
+        [Key]
+        public string Title { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        public int Episodes { get; set; }
+        public int Seasons { get; set; }
+        public int Maturity { get; set; }
+        public string Awards { get; set; }
+        public DateTime OriginalAirDate { get; set; }
+        public string Description { get; set; }
+        public int UserRating { get; set; }
+        public bool IsFamilyFriendly { get; set; }
+        public bool IsEducational { get; set; }
+
+    }
+}
