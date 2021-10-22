aeb61dcbee9e699c9e440aca8490cc9c10ef94d0
Ciara Brown <cnbrown2021@gmail.com>
2021-10-22 09:55:59 -0400

    add TvShowService

---
+++ MovieRater.Services/TvShowService.cs
@@ -0,0 +1,52 @@
+﻿using MovieRater.Data;
+using MovieRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Services
+{
+    public class TvShowService
+    {
+
+        //Create
+        public bool CreateTvShow(TvShowCreate show)
+        {
+            var entity =
+                new TvShow()
+                {
+                    Title = show.Title,
+                    Genre = show.Genre,
+                    Episodes = show.Episodes,
+                    Seasons = show.Seasons,
+                    Maturity = show.Maturity,
+                    Awards = show.Awards,
+                    OriginalAirDate = show.OriginalAirDate,
+                    Description = show.Description,
+                    UserRating = show.UserRating
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.TvShows.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+
+
+        }
+
+        //GetAll
+
+        //GetbyTitle
+
+        //GetbyUserRating
+
+
+        //Update
+
+
+        //Delete
+
+    }
+}
