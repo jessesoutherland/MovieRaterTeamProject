5374d1d92cc8e6ceed647b32bb4afb647de588ba
Ciara Brown <cnbrown2021@gmail.com>
2021-10-22 11:17:47 -0400

    add Read TvShowService

--- MovieRater.Services/TvShowService.cs
+++ MovieRater.Services/TvShowService.cs
@@ -10,6 +10,13 @@ namespace MovieRater.Services
 {
     public class TvShowService
     {
+        private readonly string _title;
+
+        public TvShowService(string title)
+        {
+            _title = title;
+        }
+            
 
         //Create
         public bool CreateTvShow(TvShowCreate show)
@@ -37,8 +44,40 @@ namespace MovieRater.Services
         }
 
         //GetAll
+        public IEnumerable<TvShowListItem> GetTvShow()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var list =
+                    ctx.TvShows.Where(l => l.Title == _title)
+                    .Select(
+                        l => new TvShowListItem
+                        {
+                            Title = l.Title,
+                            Genre = l.Genre,
+                            UserRating = l.UserRating,
+                            Maturity = l.Maturity,
+                        }
+                        );
+                return list.ToArray();
+            }
+        }
 
         //GetbyTitle
+        public TvShowListItem GetTvShowByTitle()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var tvtitle = ctx.TvShows.Single(l => l.Title == _title);
+                return new TvShowListItem
+                {
+                    Title = tvtitle.Title,
+                    Genre = tvtitle.Genre,
+                    UserRating = tvtitle.UserRating,
+                    Maturity = tvtitle.Maturity
+                };
+            }
+        }
 
         //GetbyUserRating
 
