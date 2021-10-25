5197988cb94ad8be11bb906ec786fb048cfdf24a
Ciara Brown <cnbrown2021@gmail.com>
2021-10-25 01:46:51 -0400

    update IdentityModel

--- MovieRaterTeamProject/App_Start/Startup.Auth.cs
+++ MovieRaterTeamProject/App_Start/Startup.Auth.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security.OAuth;
 using Owin;
 using MovieRaterTeamProject.Providers;
 using MovieRaterTeamProject.Models;
+using MovieRater.Data;
 
 namespace MovieRaterTeamProject
 {
